e3e343aca71d3e739a1a4d88f7cada60db7ad8ab
Jitendra Paswan <78022156+JK-101119@users.noreply.github.com>
2021-04-02 16:15:26 +0530

    First Commit

--- exerciseProgramiz.py
+++ exerciseProgramiz.py
@@ -1,1 +1,51 @@
-print("Hello From Exercise")
+# Python Program to Print Hello world!
+print("Hello World")
+
+# Python Program to Add Two Numbers
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+c = a + b
+print("Sum :", c)
+
+# Python Program to Find the Square Root
+square = lambda x : x*x
+print(square)
+
+# Python Program to Calculate the Area of a Triangle
+base = int(input("Enter base : "))
+height = int(input("Enter height : "))
+area = (height*base)/2
+print("Are of Triangle : ", area)
+
+# Python Program to Solve Quadratic Equation
+a = int(input("Enter value of ax^2 : "))
+b = int(input("Enter value of bx : "))
+c = int(input("Enter value of c : "))
+alpha = (-b+(b*b-4ac)/2a
+beeta = (-b-(b*b-4ac)/2a
+print(f"X : {alpha} Y : {beeta}")  
+         
+# Python Program to Swap Two Variables
+         a = 5
+         b = 7
+         print("a : {a} b : {b}")
+         temp = a
+         a = b
+         b = temp
+         print("a : {a} b : {b}")
+         
+# Python Program to Generate a Random Number
+         import random
+         x = random.randint(1,11)
+         print(x)
+         
+# Python Program to Convert Kilometers to Miles
+         km = int(input("Enter a KM value to convert into Miles :")
+         miles = km * 0.6
+         print(f"{km} is equal to", miles) 
+                  
+# Python Program to Convert Celsius To Fahrenheit
+         cel = float(input("Ener a value in Celsius : ")
+         far = [(9/5)*c] +32
+         print(f"{cel} is equal to {fa}")
+# Python Program to Check if a Number is Positive, Negative or 0
